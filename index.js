621d84b4d48e834e04151666eb9458a73b45d31b
Beniih <126524988+Beniih@users.noreply.github.com>
2024-05-02 10:07:55 -0300

    feat: cr calculator
    Calculador de CR baseado em DeD

--- index.js
+++ index.js
@@ -39,3 +39,88 @@ function heroLevelCalc(xp) {
       break;
   }
 }
+
+
+function heroDedCR(xp) {
+	switch(true){
+		case xp < 300:
+			heroCR = 100; //nivel 1
+			break;
+
+		case xp < 900:
+			heroCR = 200; //nivel 2
+			break;
+
+		case xp < 2700:
+			heroCR = 400; //nivel 3
+			break;
+
+		case xp < 6500:
+			heroCR = 500; //nivel 4
+			break;
+
+		case xp < 14000:
+			heroCR = 1100; //nivel 5
+			break;
+
+		case xp < 23000:
+			heroCR = 1400; //nivel 6
+			break;
+
+		case xp < 34000:
+			heroCR = 1700; //nivel 7
+			break;
+
+		case xp < 48000:
+			heroCR = 2100; //nivel 8
+			break;
+
+		case xp < 64000:
+			heroCR = 2400; //nivel 9
+			break;
+
+		case xp < 85000:
+			heroCR = 2800; //nivel 10
+			break;
+
+		case xp < 100000:
+			heroCR = 3600; //nivel 11
+			break;
+
+		case xp < 120000:
+			heroCR = 4500; //nivel 12
+			break;
+
+		case xp < 140000:
+			heroCR = 5100; //nivel 13
+			break;
+
+		case xp < 165000:
+			heroCR = 5700; //nivel 14
+			break;
+
+		case xp < 195000:
+			heroCR = 6500; //nivel 15
+			break;
+
+		case xp < 225000:
+			heroCR = 7200; //nivel 16
+			break;
+
+		case xp < 265000:
+			heroCR = 8800; //nivel 17
+			break;
+
+		case xp < 305000:
+			heroCR = 9500; //nivel 18
+			break;
+
+		case xp < 355000:
+			heroCR = 10900; //nivel 19
+			break;
+
+		default
+			heroCR = 12700; //nivel 20
+			break;
+	}
+}
